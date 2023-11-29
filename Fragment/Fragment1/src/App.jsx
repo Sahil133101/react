7cdc73dbe67c524afdca2c15bc0623e663f4f115
Sahil Sharma <103818219+Sahil133101@users.noreply.github.com>
2023-11-29 09:12:22 +0530

    fragment

---
+++ Fragment/Fragment1/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+export default function App() {
+  return(
+    <React.Fragment>
+      
+   
+    <h1> Healthy Food</h1>
+    <ul className="list-group">
+  <li class="list-group-item"><h4> DAL</h4></li>
+  <li class="list-group-item">Roti</li>
+  <li class="list-group-item">Vegetables</li>
+  <li class="list-group-item">MILK</li>
+  <li class="list-group-item">SAlAD</li>
+</ul>
+
+   
+  
+
+
+</React.Fragment>
+  );
+}
